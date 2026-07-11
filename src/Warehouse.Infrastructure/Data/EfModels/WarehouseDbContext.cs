5d41931f213579c04134ff38f2dc40c946f8592b
abed1srour <abedallatifsrour@hotmail.com>
2026-07-11 08:55:49 +0300

    feat(session-04): db first scaffolding

---
+++ src/Warehouse.Infrastructure/Data/EfModels/WarehouseDbContext.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Warehouse.Infrastructure.Data.EfModels;
+
+public partial class WarehouseDbContext : DbContext
+{
+    public WarehouseDbContext()
+    {
+    }
+
+    public WarehouseDbContext(DbContextOptions<WarehouseDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    public virtual DbSet<Productimage> Productimages { get; set; }
+
+    public virtual DbSet<Supplier> Suppliers { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseNpgsql("Host=localhost;Port=5434;Database=WarehouseDbFirst;Username=username;Password=password");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("Products_pkey");
+
+            entity.HasIndex(e => e.Sku, "Products_SKU_key").IsUnique();
+
+            entity.Property(e => e.Id).ValueGeneratedNever();
+            entity.Property(e => e.CreatedAt)
+                .HasDefaultValueSql("CURRENT_TIMESTAMP")
+                .HasColumnType("timestamp without time zone");
+            entity.Property(e => e.ExpiryDate).HasColumnType("timestamp without time zone");
+            entity.Property(e => e.IsArchived).HasDefaultValue(false);
+            entity.Property(e => e.LastUpdatedAt)
+                .HasDefaultValueSql("CURRENT_TIMESTAMP")
+                .HasColumnType("timestamp without time zone");
+            entity.Property(e => e.Name).HasMaxLength(255);
+            entity.Property(e => e.Price).HasPrecision(18, 2);
+            entity.Property(e => e.QuantityInStock).HasDefaultValue(0);
+            entity.Property(e => e.Sku)
+                .HasMaxLength(100)
+                .HasColumnName("SKU");
+            entity.Property(e => e.SupplierName).HasMaxLength(255);
+
+            entity.HasOne(d => d.Supplier).WithMany(p => p.Products)
+                .HasForeignKey(d => d.SupplierId)
+                .OnDelete(DeleteBehavior.SetNull)
+                .HasConstraintName("Products_SupplierId_fkey");
+        });
+
+        modelBuilder.Entity<Productimage>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("Productimages_pkey");
+
+            entity.Property(e => e.Id).ValueGeneratedNever();
+            entity.Property(e => e.FileName).HasMaxLength(255);
+            entity.Property(e => e.FilePath).HasMaxLength(500);
+
+            entity.HasOne(d => d.Product).WithMany(p => p.Productimages)
+                .HasForeignKey(d => d.ProductId)
+                .HasConstraintName("Productimages_ProductId_fkey");
+        });
+
+        modelBuilder.Entity<Supplier>(entity =>
+        {
+            entity.HasKey(e => e.SupplierId).HasName("Suppliers_pkey");
+
+            entity.Property(e => e.SupplierId).ValueGeneratedNever();
+            entity.Property(e => e.ContactEmail).HasMaxLength(255);
+            entity.Property(e => e.Country).HasMaxLength(100);
+            entity.Property(e => e.IsActive).HasDefaultValue(true);
+            entity.Property(e => e.Name).HasMaxLength(255);
+            entity.Property(e => e.PhoneNumber).HasMaxLength(50);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
